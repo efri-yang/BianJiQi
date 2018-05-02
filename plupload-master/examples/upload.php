08b32bbfe71f00fc762ec5eb6be30cb75d0237ee
efri-yang <948061564@qq.com>
2018-05-02 09:56:55 +0800

    no message

--- plupload-master/examples/upload.php
+++ plupload-master/examples/upload.php
@@ -9,6 +9,10 @@
  * Contributing: http://www.plupload.com/contributing
  */
 
+#!! 注意
+#!! 此文件只是个示例，不要用于真正的产品之中。
+#!! 不保证代码安全性。
+
 #!! IMPORTANT:
 #!! this file is just an example, it doesn't incorporate any security checks and
 #!! is not recommended to be used in production environment as it is. Be sure to
@@ -21,31 +25,52 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-/*
 // Support CORS
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 // other CORS headers if any...
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-exit; // finish preflight CORS requests here
+    exit; // finish preflight CORS requests here
 }
- */
+
+if (!empty($_REQUEST['debug'])) {
+    $random = rand(0, intval($_REQUEST['debug']));
+    if ($random === 0) {
+        header("HTTP/1.0 500 Internal Server Error");
+        exit;
+    }
+}
+
+// header("HTTP/1.0 500 Internal Server Error");
+// exit;
 
 // 5 minutes execution time
 @set_time_limit(5 * 60);
-
+// 设置脚本最大执行时间
+//
+//
 // Uncomment this one to fake upload time
 // usleep(5000);
 
 // Settings
 // $targetDir = ini_get("upload_tmp_dir") . DIRECTORY_SEPARATOR . "plupload";
-$targetDir = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "BianJiQi" . DIRECTORY_SEPARATOR . "uploads";
-//$targetDir = 'uploads';
-$cleanupTargetDir = true; // Remove old files
+// 虚拟文件存放的目录
+$targetDir = 'upload_tmp';
+
+//文件上传后的目录
+$uploadDir = 'upload';
+
+$cleanupTargetDir = false; // Remove old files
 $maxFileAge = 5 * 3600; // Temp file age in seconds
 
 // Create target dir
+// 文件目录不存在 那么就创建一个
 if (!file_exists($targetDir)) {
     @mkdir($targetDir);
+}
+
+// Create target dir
+if (!file_exists($uploadDir)) {
+    @mkdir($uploadDir);
 }
 
 // Get a file name
@@ -57,13 +82,19 @@ if (isset($_REQUEST["name"])) {
     $fileName = uniqid("file_");
 }
 
-$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+$oldName = $fileName;
 
+$filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+$uploadPath = $uploadDir . DIRECTORY_SEPARATOR . $fileName;
+//DIRECTORY_SEPARATOR>\  就是反斜杠的意思
 // Chunking might be enabled
+// 获取变量的整数值
 $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
-$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+$chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 1;
 
 // Remove old temp files
+//
+//  打开目录句柄 opendir
 if ($cleanupTargetDir) {
     if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
         die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
@@ -73,12 +104,12 @@ if ($cleanupTargetDir) {
         $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
 
         // If temp file is current file proceed to the next
-        if ($tmpfilePath == "{$filePath}.part") {
+        if ($tmpfilePath == "{$filePath}_{$chunk}.part" || $tmpfilePath == "{$filePath}_{$chunk}.parttmp") {
             continue;
         }
 
         // Remove temp file if it is older than the max age and is not the current file
-        if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+        if (preg_match('/\.(part|parttmp)$/', $file) && (@filemtime($tmpfilePath) < time() - $maxFileAge)) {
             @unlink($tmpfilePath);
         }
     }
@@ -86,7 +117,7 @@ if ($cleanupTargetDir) {
 }
 
 // Open temp file
-if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
+if (!$out = @fopen("{$filePath}_{$chunk}.parttmp", "wb")) {
     die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
 }
 
@@ -96,6 +127,8 @@ if (!empty($_FILES)) {
     }
 
     // Read binary input stream and append it to temp file
+    // http://www.cnblogs.com/Romi/archive/2012/02/29/2374769.html
+    // 在区分二进制文件和文本文件的系统上（如 Windows）打开文件时，fopen() 函数的 mode 参数要加上 'b'。
     if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
         die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
     }
@@ -112,12 +145,52 @@ while ($buff = fread($in, 4096)) {
 @fclose($out);
 @fclose($in);
 
-// Check if file has been uploaded
-if (!$chunks || $chunk == $chunks - 1) {
-    // Strip the temp .part suffix off
-    rename("{$filePath}.part", $filePath);
+rename("{$filePath}_{$chunk}.parttmp", "{$filePath}_{$chunk}.part");
+
+$index = 0;
+$done = true;
+for ($index = 0; $index < $chunks; $index++) {
+    if (!file_exists("{$filePath}_{$index}.part")) {
+        $done = false;
+        break;
+    }
+}
+if ($done) {
+    $pathInfo = pathinfo($fileName);
+    if (!$out = @fopen($uploadPath, "wb")) {
+        die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+    }
+    // flock — 轻便的咨询文件锁定   取得独占锁定（写入的程序。
+    if (flock($out, LOCK_EX)) {
+        for ($index = 0; $index < $chunks; $index++) {
+            if (!$in = @fopen("{$filePath}_{$index}.part", "rb")) {
+                break;
+            }
+
+            while ($buff = fread($in, 4096)) {
+                fwrite($out, $buff);
+            }
+
+            @fclose($in);
+            //删除文件01.jpg_0.part
+            // @unlink("{$filePath}_{$index}.part");
+        }
+
+        flock($out, LOCK_UN);
+    }
+    @fclose($out);
+
+    $response = [
+        'success' => true,
+        'oldName' => $oldName,
+        'filePath' => "http://localhost/BianJiQi/plupload-master/examples/" . $uploadDir . "/" . $fileName,
+        'fileSize' => $_REQUEST['size'],
+        'fileSuffixes' => $pathInfo['extension'],
+        'file_id' => $_REQUEST['id'],
+    ];
+
+    echo $response["filePath"];
 }
 
 // Return Success JSON-RPC response
-echo $filePath;
-// die('{"jsonrpc" : "2.0", "result" :, "id" : "id"}');
+// die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
