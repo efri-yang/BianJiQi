47e2e5203376d7acf7fc46b6e718cda6860a6f19
efri-yang <948061564@qq.com>
2018-05-03 10:24:28 +0800

    c

--- ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/image/plugin.js
+++ ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/image/plugin.js
@@ -1,32 +1,323 @@
-﻿CKEDITOR.plugins.add( 'image', {
-	icons: 'image',
-	init: function( editor ) {
-		
-		editor.ui.addButton( 'image', {
+﻿CKEDITOR.plugins.add('image', {
+    icons: 'image',
+    init: function(editor) {
 
-			// The text part of the button (if available) and the tooltip.
-			label: '上传图片',
+        editor.ui.addButton('image', {
 
-			// The command to execute on click.
-			command: 'image',
+            // The text part of the button (if available) and the tooltip.
+            label: '上传图片',
 
-			// The button placement in the toolbar (toolbar group name).
-			toolbar: 'insert'
-		});
+            // The command to execute on click.
+            command: 'image',
 
-		
-		
-		
-		
+            // The button placement in the toolbar (toolbar group name).
+            toolbar: 'insert'
+        });
 
-		// CKEDITOR.dialog.add( 'imageDialog', this.path + 'dialogs/image.js' );
-		editor.addCommand( 'image', {
-		    exec: function( editor ) {
-		       	
-		    	
 
-		    }
-		} );
-	}
+        editor.layerIndex="";
+// 
+
+
+
+
+        // CKEDITOR.dialog.add( 'imageDialog', this.path + 'dialogs/image.js' );
+        editor.addCommand('image', {
+            exec: function(editor) {
+
+            	 editor.layerIndex=layer.open({
+			          type: 1,
+			          shade: false,
+			          move: ".move-seat",
+			          title: false, //不显示标题
+			          area: '702px',
+			          content: $(".ckeditor-imgupload-dialog"), //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+			          success: function(layero, index){
+			           
+			          },
+			          cancel: function(index, layero){ 
+			            
+			          }
+        		});
+
+                var uploader = new plupload.Uploader({
+                    runtimes: 'html5,flash,silverlight,html4',
+                    browse_button: 'img-add-btn-1', // you can pass an id...
+                    url: 'upload2.php',
+                    flash_swf_url: '../js/Moxie.swf',
+                    silverlight_xap_url: '../js/Moxie.xap',
+                    filters: {
+                        prevent_duplicates: true,
+                        max_file_size: '1000mb',
+                        mime_types: [
+                            { title: "Image files", extensions: "jpg,gif,png,psd" },
+                            { title: "Zip files", extensions: "zip" }
+                        ]
+                    }
+                });
+
+
+                var $uploadBtn = $(".img-upload-btn");
+                var $addBtn = $("#img-add-btn-1");
+                var $insertBtn = $(".ft-btn-insert");
+                var $fileListUL = $(".ckeditor-uploadfile-list");
+                $(".ft-btn-cancel").on("click",function(){
+                	layer.close(editor.layerIndex);
+                });
+
+                $('.ckeditor-imgupload-dialog .imgupload-tab-hd a').click(function(e) {
+		            e.preventDefault();
+
+		            $(this).editorTab('show');
+		           
+		        });
+
+                $insertBtn.on("click",function(){	
+                	$fileListUL.children("li").each(function(index, el) {
+                		var $el=$(el);
+                		var src=$el.attr("data-src");
+                		if(!!src){
+	                		var img = editor.document.createElement( 'img' );
+							img.setAttribute( 'src',src);
+	                		editor.insertElement( img );
+	                		$el.remove();
+	                		layer.close(editor.layerIndex);
+	      					
+                		}
+                	});
+                });
+
+                uploader.bind("Init", function(uploader) {
+                    console.group("Init事件:当Plupload初始化完成后触发监听函数参数：(uploader)");
+                });
+
+                uploader.bind("PostInit", function() {
+                    console.group("PostInit事件:当Init事件发生后触发监听函数参数：(uploader)");
+                    $(".img-upload-btn").on("click", function() {
+                        uploader.start();
+                    });
+                });
+
+                uploader.bind("Browse", function(up) {
+                    console.group("Browse事件")
+                });
+
+
+
+                uploader.bind('FileFiltered', function(up, file) {
+                    console.group("FileFiltered事件")
+                });
+
+
+
+
+                function preloadThumb(file, cb) {
+                    var img = new moxie.image.Image();
+                    var resolveUrl = moxie.core.utils.Url.resolveUrl;
+                    img.onload = function() {
+                        var thumb = $('#' + file.id);
+                        thumb.find(".img-before-preview").hide();
+                        this.embed(thumb[0], {
+                            // width: self.options.thumb_width,
+                            // height: self.options.thumb_height,
+                            width: 110,
+                            height: 110,
+                            crop: false,
+                            fit: true,
+                            preserveHeaders: false,
+                            swf_url: resolveUrl(uploader.settings.flash_swf_url),
+                            xap_url: resolveUrl(uploader.settings.silverlight_xap_url)
+                        });
+                    };
+
+                    img.bind("embedded error", function(e) {
+                        //不支持的浏览器就会触发error事件,支持的浏览器会触发embedded  e.type
+                        this.destroy();
+                        setTimeout(function() {
+                            cb.call(null, e.type)
+                        }, 1); // detach, otherwise ui might hang (in SilverLight for example)
+                    });
+
+                    $('#' + file.id).removeClass('plupload_thumb_toload').addClass('plupload_thumb_loading');
+                    img.load(file.getSource());
+                }
+
+
+
+
+                uploader.bind('FilesAdded', function(up, files) {
+                    console.group("FilesAdded事件");
+                    console.dir(files);
+                    $(".img-upload-btn").removeClass("disabled");
+
+                    var str = "";
+                    var $beforePreview;
+                    var $afterPreview;
+                    plupload.each(files, function(file) {
+                        var str = '<li id="' + file.id + '">' +
+                            '<div class="img-before-preview">' +
+                            '<p class="title">' + file.name + '</p>' +
+                            '<p class="txt-error-tip"></p>' +
+                            '<p class="img-loading"></p>' +
+                            '</div>' +
+                            '<div class="img-after-preview">' +
+                            '<img src="" class="img-upload" />' +
+                            '</div>' +
+                            '<div class="img-progress"><span></span></div>' +
+                            '<a href="#" class="img-del-btn"></a>' +
+                            '<span class="img-success"></span>' +
+                            '<span class="img-error">上传失败，请重试</span>' +
+                            '</li>';
+                        var $str = $(str);
+                        $str.appendTo($fileListUL);
+                        $beforePreview = $str.find('.img-before-preview');
+                        $afterPreview = $str.find(".img-after-preview");
+                        $del = $str.find(".img-del-btn");
+
+                        $del.show();
+                        $del.on("click", function() {
+                            uploader.removeFile(file);
+                            console.dir(uploader.files);
+
+
+
+                        })
+
+                        preloadThumb(file, function(type) {
+                            if (type == "error") {
+                                $str.find(".img-loading").hide();
+                                $str.find(".txt-error-tip").text("该浏览器不支持图片预览").show();
+
+                            } else {
+                                $beforePreview.hide();
+                            }
+
+
+
+                        })
+                    });
+                });
+
+
+
+
+
+                uploader.bind('QueueChanged', function(up) {
+                    console.group("QueueChanged事件");
+
+                });
+
+                uploader.bind('Refresh', function(up) {
+                    console.group("Refresh事件");
+                    if (!up.total.queued) {
+                        $insertBtn.addClass("disabled");
+                    } else {
+                        $insertBtn.removeClass("disabled");
+                    }
+                });
+
+
+
+
+                uploader.bind('BeforeUpload', function(up, file) {
+                    console.group("BeforeUpload事件");
+                    console.dir(file);
+                    var $li = $("#" + file.id);
+                    var $propress = $li.find(".img-progress").show();
+                    $li.find(".img-del-btn").hide();
+                });
+                uploader.bind('UploadProgress', function(up, file) {
+                    console.group("UploadProgress事件");
+                    console.dir(file);
+                    var $li = $("#" + file.id);
+                    var $propress = $li.find(".img-progress").show();
+                    $propress.children('span').css("width", file.percent + "%");
+
+                });
+
+
+
+
+                uploader.bind('FileUploaded', function(up, file, info) {
+                    console.dir(info);
+                    console.group("FileUploaded事件");
+                    var $li = $("#" + file.id);
+                    var $propress = $li.find(".img-progress").hide();
+                    $li.find(".img-success").show();
+                    $li.attr("data-src", info.response);
+                    $li.find(".img-del-btn").show();
+
+
+                });
+
+                uploader.bind('StateChanged', function(up) {
+                    //当前的上传状态，可能的值为plupload.STARTED或plupload.STOPPED，该值由Plupload实例的stop()或statr()方法控制。默认为plupload.STOPPED
+                    console.group("StateChanged事件");
+                    console.dir(up.state);
+                });
+
+                uploader.bind('UploadComplete', function(up, files) {
+                    console.group("UploadComplete事件");
+
+                });
+
+
+
+
+
+
+                uploader.bind('FilesRemoved', function(up, files) {
+                    console.group("FilesRemoved事件");
+
+                    $.each(files, function(index, file) {
+
+                        $("#" + file.id).remove();
+
+                    });
+
+
+                    setTimeout(function() {
+
+                        if (!uploader.total.queued) {
+                            $uploadBtn.addClass("disabled");
+                        }
+                    })
+
+
+
+                });
+
+                uploader.bind('ChunkUploaded', function(up, file, info) {
+                    console.group("ChunkUploaded事件")
+                });
+
+                uploader.bind('Destroy', function() {
+                    console.group("Destroy事件")
+                });
+
+                uploader.bind('OptionChanged', function(up, name, value, oldValue) {
+                    console.group("OptionChanged事件")
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                uploader.init();
+
+
+            }
+        });
+    }
 
 });
--- ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/simplebox/plugin.js
+++ ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/simplebox/plugin.js
@@ -2,10 +2,10 @@
  * Copyright (c) 2014-2018, CKSource - Frederico Knabben. All rights reserved.
  * Licensed under the terms of the MIT License (see LICENSE.md).
  *
- * Simple CKEditor Widget (Part 2).
+ * Simple CKEditor Widget (Part 1).
  *
  * Created out of the CKEditor Widget SDK:
- * http://docs.ckeditor.com/ckeditor4/docs/#!/guide/widget_sdk_tutorial_2
+ * https://docs.ckeditor.com/ckeditor4/docs/#!/guide/widget_sdk_tutorial_1
  */
 
 // Register the plugin within the editor.
@@ -19,18 +19,13 @@ CKEDITOR.plugins.add( 'simplebox', {
 
 	// The plugin initialization logic goes inside this method.
 	init: function( editor ) {
-		// Register the editing dialog.
-		CKEDITOR.dialog.add( 'simplebox', this.path + 'dialogs/simplebox.js' );
-
 		// Register the simplebox widget.
 		editor.widgets.add( 'simplebox', {
-			// Allow all HTML elements, classes, and styles that this widget requires.
+			// Allow all HTML elements and classes that this widget requires.
 			// Read more about the Advanced Content Filter here:
-			// * http://docs.ckeditor.com/ckeditor4/docs/#!/guide/dev_advanced_content_filter
-			// * http://docs.ckeditor.com/ckeditor4/docs/#!/guide/plugin_sdk_integration_with_acf
-			allowedContent:
-				'div(!simplebox,align-left,align-right,align-center){width};' +
-				'div(!simplebox-content); h2(!simplebox-title)',
+			// * https://docs.ckeditor.com/ckeditor4/docs/#!/guide/dev_advanced_content_filter
+			// * https://docs.ckeditor.com/ckeditor4/docs/#!/guide/plugin_sdk_integration_with_acf
+			allowedContent: 'div(!simplebox); div(!simplebox-content); h2(!simplebox-title)',
 
 			// Minimum HTML which is required by this widget to work.
 			requiredContent: 'div(simplebox)',
@@ -38,7 +33,7 @@ CKEDITOR.plugins.add( 'simplebox', {
 			// Define two nested editable areas.
 			editables: {
 				title: {
-					// Define CSS selector used for finding the element inside widget element.
+					// Define a CSS selector used for finding the element inside the widget element.
 					selector: '.simplebox-title',
 					// Define content allowed in this nested editable. Its content will be
 					// filtered accordingly and the toolbar will be adjusted when this editable
@@ -68,56 +63,15 @@ CKEDITOR.plugins.add( 'simplebox', {
 			// editor.lang.simplebox.* property. A string was used directly here to simplify this tutorial.
 			button: 'Create a simple box',
 
-			// Set the widget dialog window name. This enables the automatic widget-dialog binding.
-			// This dialog window will be opened when creating a new widget or editing an existing one.
-			dialog: 'simplebox',
-
 			// Check the elements that need to be converted to widgets.
 			//
-			// Note: The "element" argument is an instance of http://docs.ckeditor.com/ckeditor4/docs/#!/api/CKEDITOR.htmlParser.element
+			// Note: The "element" argument is an instance of http://docs.ckeditor.com/#!/api/CKEDITOR.htmlParser.element
 			// so it is not a real DOM element yet. This is caused by the fact that upcasting is performed
 			// during data processing which is done on DOM represented by JavaScript objects.
 			upcast: function( element ) {
 				// Return "true" (that element needs to converted to a Simple Box widget)
 				// for all <div> elements with a "simplebox" class.
 				return element.name == 'div' && element.hasClass( 'simplebox' );
-			},
-
-			// When a widget is being initialized, we need to read the data ("align" and "width")
-			// from DOM and set it by using the widget.setData() method.
-			// More code which needs to be executed when DOM is available may go here.
-			init: function() {
-				var width = this.element.getStyle( 'width' );
-				alert("asdfasdfasdf");
-				if ( width )
-					this.setData( 'width', width );
-
-				if ( this.element.hasClass( 'align-left' ) )
-					this.setData( 'align', 'left' );
-				if ( this.element.hasClass( 'align-right' ) )
-					this.setData( 'align', 'right' );
-				if ( this.element.hasClass( 'align-center' ) )
-					this.setData( 'align', 'center' );
-			},
-
-			// Listen on the widget#data event which is fired every time the widget data changes
-			// and updates the widget's view.
-			// Data may be changed by using the widget.setData() method, which we use in the
-			// Simple Box dialog window.
-			data: function() {
-				// Check whether "width" widget data is set and remove or set "width" CSS style.
-				// The style is set on widget main element (div.simplebox).
-				if ( this.data.width == '' )
-					this.element.removeStyle( 'width' );
-				else
-					this.element.setStyle( 'width', this.data.width );
-
-				// Brutally remove all align classes and set a new one if "align" widget data is set.
-				this.element.removeClass( 'align-left' );
-				this.element.removeClass( 'align-right' );
-				this.element.removeClass( 'align-center' );
-				if ( this.data.align )
-					this.element.addClass( 'align-' + this.data.align );
 			}
 		} );
 	}
