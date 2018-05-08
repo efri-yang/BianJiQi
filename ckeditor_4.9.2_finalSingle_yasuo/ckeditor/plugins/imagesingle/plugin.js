1701a25ab41418d6530de575dce7cf71db06e0d2
efri-yang <948061564@qq.com>
2018-05-08 13:23:23 +0800

    c

--- ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/imagesingle/plugin.js
+++ ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/imagesingle/plugin.js
@@ -23,14 +23,16 @@ CKEDITOR.plugins.add('imagesingle', {
 
         setTimeout(function() {
             var uploader, browseBtnId = editor.name + "-imagesingle-upload";
-            var strBrowse = '<a href="javascript:void(0);" id="' + browseBtnId + '" style="position:absolute;left:0;right: 0;top:0;bottom: 0;z-index:10;"></a>'
+            // var strBrowse = '<a href="javascript:void(0);" id="' + browseBtnId + '" style="width:60px;height:60px;background:#f60;display:block;"></a>'
+            
+             var strBrowse = '<a href="javascript:void(0);" id="' + browseBtnId + '" style="position:absolute;left:0;right:0;top:0;bottom:0;background:#f60;z-index:10;"></a>'
 
 
             $(strBrowse).appendTo($('.cke_button__imagesingle_icon'));
 
             uploader = new plupload.Uploader({
                 runtimes: 'html5,flash,silverlight,html4',
-                browse_button: browseBtnId, // you can pass an id...
+                browse_button:browseBtnId, // you can pass an id...
                 url: 'upload.php',
                 flash_swf_url: '../js/Moxie.swf',
                 silverlight_xap_url: '../js/Moxie.xap',
@@ -63,7 +65,7 @@ CKEDITOR.plugins.add('imagesingle', {
             
             editor.addCommand('imagesingle', {
                 exec: function(editor) {
-                    $("#" + browseBtnId).click();
+                    $("#"+browseBtnId).trigger('click');
                 }
             })
         })
