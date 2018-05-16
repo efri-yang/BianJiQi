6fa13ee4957b993097bad1e74da086407b23b383
efri-yang <948061564@qq.com>
2018-05-16 16:31:15 +0800

    c

--- ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/smileybox/plugin.js
+++ ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/smileybox/plugin.js
@@ -19,9 +19,44 @@
 
             editor.addCommand('smileybox', {
                 exec: function(editor) {
-                	alert("sdfsadfasdfasdf");
+                	if(!editor.smiley.$dialog){
+                        renderDialogHtml.call(editor);
+                    }
+
+                    editor.smiley.layerIndex = layer.open({
+                        type: 1,
+                        shade: false,
+                        title: false, //不显示标题
+                        area: "420px",
+                        content: editor.smiley.$dialog, //捕获的元素，注意：最好该指定的元素要存放在body最外层，否则可能被其它的相对元素所影响
+                        success: function(layero, index) {
+                            
+                        },
+                        cancel: function(index, layero) {
+
+                        }
+                    })
                 }
             });
         }
-    })
+    });
+
+    function renderDialogHtml(){
+        var editor=this;
+        var str='<div class="ckeditor-smiley-dialog" id="' + editor.smiley.dialogId + '">'+
+                    '<ul class="smiley-hd-list">'+
+                        '<li class="active"><a href="#">默认表情</a></li>'+
+                        '<li><a href="#">小鱼</a></li>'+
+                        '<li><a href="#">免司机</a></li>'+
+                        '<li class="move-seat"></li>'+
+                    '</ul>'+
+                    '<div class="smiley-bd">'+
+                        '<ul class="smiley-pic-list">'+
+                            '<li><img src="./images/demo/angel_smile.gif"></li><li><img src="./images/demo/angel_smile.gif"></li>'+
+                        '</ul>'+
+                    '</div>'+
+                '<div>';
+        $(str).appendTo($("body"));
+        editor.smiley.$dialog = $("#" + editor.smiley.dialogId);
+    }
 })()
