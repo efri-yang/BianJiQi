858d484c14fa4f52825749bc0dc908484dd48872
efri-yang <948061564@qq.com>
2018-05-16 13:38:54 +0800

    c

--- ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/smileybox/plugin.js
+++ ckeditor_4.9.2_finalSingle_yasuo/ckeditor/plugins/smileybox/plugin.js
@@ -1,3 +1,27 @@
-CKEDITOR.plugins.add('imagesingle', {
-	icons: 'imagesingle',
-})
+(function() {
+    CKEDITOR.plugins.add('smileybox', {
+        icons: 'smileybox',
+        beforeInit: function(editor) {
+            editor.smiley = {};
+            editor.smiley.dialogId = editor.name + "-smiley-dialog";
+            editor.smiley.initialLinkText = "";
+        },
+        init: function() {
+            editor.ui.addButton('smileybox', {
+                // The text part of the button (if available) and the tooltip.
+                label: '表情符号',
+                // The command to execute on click.
+                command: 'smileybox',
+                // The button placement in the toolbar (toolbar group name).
+                toolbar: 'insert,2'
+            });
+
+
+            editor.addCommand('smileybox', {
+                exec: function(editor) {
+                	alert("sdfsadfasdfasdf");
+                }
+            });
+        }
+    })
+})()
